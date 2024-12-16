2ec1f1e0fe96bacf4c48a382499f52a3b25f8111
njxxt <vovchenko.vladimir@phystech.edu>
2024-12-16 04:20:29 +0300

    1.py

--- ласт семинар/1.py
+++ ласт семинар/1.py
@@ -30,20 +30,11 @@ def prost(n):
 ##################
 
 class test_numbers(unittest.TestCase):
-    def test_1(self):
-        self.assertEqual(prost(1), [])
-
     def test_2(self):
         self.assertEqual(prost(2), [2])
 
-    def test_3(self):
-        self.assertEqual(prost(3), [3])
-
     def test_4(self):
         self.assertEqual(prost(4), [2, 2])
-
-    def test_18(self):
-        self.assertEqual(prost(18), [2, 3, 3])
 
     def test_29(self):
         self.assertEqual(prost(29), [29])
@@ -53,14 +44,6 @@ class test_numbers(unittest.TestCase):
 
     def test_12345(self):
         self.assertEqual(prost(12345), [3, 5, 823])
-
-    def test_neg(self):
-        with self.assertRaises(ValueError):
-            prost(-10)
-
-    def test_0(self):
-        with self.assertRaises(ValueError):
-            prost(0)
 
 if __name__ == "__main__":
     unittest.main()
